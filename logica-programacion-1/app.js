6a6e2d5e23672611a60fcd92d35ceb5eb08a78eb
Roman Rios <romanrios@live.com>
2024-07-02 03:10:48 -0300

    Initial commit

---
+++ logica-programacion-1/app.js
@@ -0,0 +1,30 @@
+let numeroMaximoPosible = 50;
+let numeroSecreto = Math.ceil(Math.random() * numeroMaximoPosible);
+let numeroUsuario;
+let intentos = 0;
+let maximosIntentos = 3;
+
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = parseInt(
+    prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible} por favor:`)
+  );
+  intentos++;
+  if (numeroUsuario == numeroSecreto) {
+    alert(
+      `Acertaste, el número es ${numeroUsuario}. Lo hiciste en ${intentos} ${
+        intentos == 1 ? "intento" : "intentos"
+      }.`
+    );
+    break;
+  } else {
+    if (numeroSecreto > numeroUsuario) {
+      alert("No acertaste, el número secreto es mayor.");
+    } else {
+      alert("No acertaste, el número secreto es menor.");
+    }
+  }
+  if (intentos >= maximosIntentos) {
+    alert(`Llegaste al máximo de ${maximosIntentos} intentos.`);
+    break;
+  }
+}
