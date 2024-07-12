046672f868d9d99cb571532bb357fb79ed8b2987
Roman Rios <romanrios@live.com>
2024-07-12 19:29:53 -0300

    javascript_v1

--- challenge-encriptador/js/main.js
+++ challenge-encriptador/js/main.js
@@ -2,19 +2,41 @@ function showResultEncrypt() {
   const outputSection = document.querySelector("#main__output");
   const textareaText = document.querySelector("#main__input__textarea").value;
   outputSection.innerHTML = `
-      <p class="result">${textareaText}</p>
-      <button onclick="copyToClipboard('${textareaText}')">Copiar</button>
+      <p id="result__text" class="result__text">${encrypt(textareaText)}</p>
+      <button class="result__button" onclick="copyToClipboard()">Copiar</button>
     `;
 }
 
-function showResultDecrypt(result) {
+function showResultDecrypt() {
   const outputSection = document.querySelector("#main__output");
+  const textareaText = document.querySelector("#main__input__textarea").value;
   outputSection.innerHTML = `
-        <p class="result">${result}</p>
-        <button onclick="copyToClipboard('${result}')">Copiar</button>
+        <p id="result__text" class="result__text">${decrypt(textareaText)}</p>
+        <button class="result__button" onclick="copyToClipboard()">Copiar</button>
       `;
 }
 
-function copyToClipboard(text) {
-  alert(text);
+function copyToClipboard() {
+  const textToCopy = document.getElementById("result__text").textContent;
+  navigator.clipboard.writeText(textToCopy);
+  alert("Texto copiado al portapapeles");
+}
+
+function encrypt(text) {
+  return text
+    .toLowerCase()
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+}
+
+function decrypt(text) {
+  return text
+    .replace(/ufat/g, "u")
+    .replace(/ober/g, "o")
+    .replace(/ai/g, "a")
+    .replace(/imes/g, "i")
+    .replace(/enter/g, "e");
 }
