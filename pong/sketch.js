c488c500445acfa147ccb8ea6b4a3754f88b19cc
Román Ríos <romanrios@live.com>
2024-10-12 20:37:45 -0300

    pong v2.0

--- pong/sketch.js
+++ pong/sketch.js
@@ -1,20 +1,40 @@
+let ball;
+let playerPaddle;
+let computerPaddle;
 let ballX, ballY, ballSpeedX, ballSpeedY, ballRadius = 10;
 let paddleWidth = 10, paddleHeight = 100;
-let playerPaddleY, computerPaddleY;
+let playerPaddleY = 0;
+let computerPaddleY = 0;
 let playerScore = 0, computerScore = 0;
 let particles = [];
 let trail = [];
 let explosionParticles = [];
+let bg;
+let bounceSound, gameOverSound, scoreSound;
+let gameEnded = false;
+let inputBlocked = false;
+let gameOverMessage = "";
+let gameOverTimer = 0;
+let ballAngle = 0;
+
+function preload() {
+  ball = loadImage('assets/bola.png');
+  playerPaddle = loadImage('assets/barra1.png');
+  computerPaddle = loadImage('assets/barra2.png');
+  bg = loadImage('assets/fondo1.png');
+  bounceSound = loadSound('assets/bounce.wav');
+  gameOverSound = loadSound('assets/gameover.wav');
+  scoreSound = loadSound('assets/score.mp3');
+}
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(windowWidth, windowHeight);
   resetBall();
-  playerPaddleY = height / 2 - paddleHeight / 2;
-  computerPaddleY = height / 2 - paddleHeight / 2;
 }
 
 function draw() {
-  background(0);
+  imageMode(CORNER);
+  image(bg, 0, 0, width, height);
   
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
@@ -39,72 +59,114 @@ function draw() {
     }
   }
 
-  noStroke();
-  fill(255);
-  ellipse(ballX, ballY, ballRadius * 2);
-  rect(20, playerPaddleY, paddleWidth, paddleHeight);
-  rect(width - 30, computerPaddleY, paddleWidth, paddleHeight);
+  imageMode(CENTER);
+  if (!gameEnded) {
+    ballAngle += (abs(ballSpeedX) + abs(ballSpeedY)) * 0.01;
+    push();
+    translate(ballX, ballY);
+    rotate(ballAngle);
+    image(ball, 0, 0, ballRadius * 2, ballRadius * 2);
+    pop();
+  }
+  image(playerPaddle, 25, playerPaddleY + paddleHeight / 2, paddleWidth, paddleHeight);
+  image(computerPaddle, width - 25, computerPaddleY + paddleHeight / 2, paddleWidth, paddleHeight);
   
   textSize(128);
-  fill(150);
+  fill(255, 150);
   textAlign(CENTER, CENTER);
   text(playerScore, width / 4, height / 2);
   text(computerScore, 3 * width / 4, height / 2);
   
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
+  if (!gameEnded) {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
 
-  if (ballY < ballRadius || ballY > height - ballRadius) {
-    ballSpeedY *= -1;
-    createParticles(ballX, ballY);
-  }
+    if (ballY < ballRadius || ballY > height - ballRadius) {
+      ballSpeedY *= -1;
+      bounceSound.play();
+      createParticles(ballX, ballY);
+    }
 
-  if (ballX - ballRadius < 30 && ballY > playerPaddleY && ballY < playerPaddleY + paddleHeight) {
-    ballSpeedX *= -1;
-    ballX = 30 + ballRadius;
-    let deltaY = ballY - (playerPaddleY + paddleHeight / 2);
-    ballSpeedY = deltaY * 0.35;
-    ballSpeedX *= 1.1;
-    ballSpeedY *= 1.1;
-    createParticles(ballX, ballY);
-  }
+    if (ballX - ballRadius < 30 && ballY > playerPaddleY && ballY < playerPaddleY + paddleHeight) {
+      ballSpeedX *= -1;
+      bounceSound.play();
+      ballX = 30 + ballRadius;
+      let deltaY = ballY - (playerPaddleY + paddleHeight / 2);
+      ballSpeedY = deltaY * 0.35;
+      ballSpeedX *= 1.1;
+      ballSpeedY *= 1.1;
+      createParticles(ballX, ballY);
+    }
 
-  if (ballX + ballRadius > width - 30 && ballY > computerPaddleY && ballY < computerPaddleY + paddleHeight) {
-    ballSpeedX *= -1;
-    ballX = width - 30 - ballRadius;
-    let deltaY = ballY - (computerPaddleY + paddleHeight / 2);
-    ballSpeedY = deltaY * 0.35;
-    ballSpeedX *= 1.1;
-    ballSpeedY *= 1.1;
-    createParticles(ballX, ballY);
-  }
+    if (ballX + ballRadius > width - 30 && ballY > computerPaddleY && ballY < computerPaddleY + paddleHeight) {
+      ballSpeedX *= -1;
+      bounceSound.play();
+      ballX = width - 30 - ballRadius;
+      let deltaY = ballY - (computerPaddleY + paddleHeight / 2);
+      ballSpeedY = deltaY * 0.35;
+      ballSpeedX *= 1.1;
+      ballSpeedY *= 1.1;
+      createParticles(ballX, ballY);
+    }
 
-  if (ballX < 0) {
-    computerScore++;
-    createExplosion(30, height / 2);
-    resetBall();
-  } else if (ballX > width) {
-    playerScore++;
-    createExplosion(width - 30, height / 2);
-    resetBall();
-  }
+    if (ballX < 0) {
+      computerScore++;
+      createExplosion(30, height / 2);
+      scoreSound.play();
+      resetBall();
+    } else if (ballX > width) {
+      playerScore++;
+      createExplosion(width - 30, height / 2);
+      scoreSound.play();
+      resetBall();
+    }
 
-  if (keyIsDown(UP_ARROW) && playerPaddleY > 0) {
-    playerPaddleY -= 6;
-  }
-  if (keyIsDown(DOWN_ARROW) && playerPaddleY < height - paddleHeight) {
-    playerPaddleY += 6;
-  }
+    if (playerScore === 10) {
+      gameEnded = true;
+      gameOverSound.play();
+      gameOverMessage = "YOU WIN";
+      blockInput();
+      gameOverTimer = millis();
+    } else if (computerScore === 10) {
+      gameEnded = true;
+      gameOverSound.play();
+      gameOverMessage = "YOU LOSE";
+      blockInput();
+      gameOverTimer = millis();
+    }
 
-  let computerError = random(-15, 15);
-  if (ballY > computerPaddleY + paddleHeight / 2 + computerError) {
-    computerPaddleY += min(3, ballY - computerPaddleY - paddleHeight / 2);
+    if (!gameEnded && !inputBlocked) {
+      if (keyIsDown(UP_ARROW) && playerPaddleY > 0) {
+        playerPaddleY -= 6;
+      }
+      if (keyIsDown(DOWN_ARROW) && playerPaddleY < height - paddleHeight) {
+        playerPaddleY += 6;
+      }
+
+      let computerError = random(-15, 15);
+      if (ballY > computerPaddleY + paddleHeight / 2 + computerError) {
+        computerPaddleY += min(3, ballY - computerPaddleY - paddleHeight / 2);
+      } else {
+        computerPaddleY -= min(3, computerPaddleY - ballY + paddleHeight / 2);
+      }
+      computerPaddleY = constrain(computerPaddleY, 0, height - paddleHeight);
+      
+      trail.push(new Trail(ballX, ballY));
+    }
   } else {
-    computerPaddleY -= min(3, computerPaddleY - ballY + paddleHeight / 2);
+    if (millis() - gameOverTimer < 3000) {
+      textSize(64);
+      fill(255, 255, 255, 200);
+      textAlign(CENTER, CENTER);
+      text(gameOverMessage, width / 2, height / 2);
+    } else {
+      resetGame();
+    }
   }
-  computerPaddleY = constrain(computerPaddleY, 0, height - paddleHeight);
+}
 
-  trail.push(new Trail(ballX, ballY));
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function resetBall() {
@@ -112,6 +174,7 @@ function resetBall() {
   ballY = height / 2;
   ballSpeedX = random(4, 6) * (random() > 0.5 ? 1 : -1);
   ballSpeedY = random(3, 5) * (random() > 0.5 ? 1 : -1);
+  ballAngle = 0;
 }
 
 function createParticles(x, y) {
@@ -121,21 +184,42 @@ function createParticles(x, y) {
 }
 
 function createExplosion(x, y) {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 100; i++) {
     explosionParticles.push(new Particle(x, y, true));
   }
+}
+
+function keyPressed() {
+  if (gameEnded && !inputBlocked) {
+    resetGame();
+  }
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  gameEnded = false;
+  resetBall();
+}
+
+function blockInput() {
+  inputBlocked = true;
+  setTimeout(() => {
+    inputBlocked = false;
+  }, 2000);
 }
 
 class Particle {
   constructor(x, y, isExplosion = false) {
     this.x = x;
     this.y = y;
-    this.size = random(5, 10);
+    this.size = random(3, 5);
     this.speedX = random(-2, 2);
     this.speedY = random(-2, 2);
     this.lifespan = 255;
+    this.color = color(random(255), random(255), random(255));
     if (isExplosion) {
-      this.size = random(10, 20);
+      this.size = random(5, 10);
       this.speedX *= 3;
       this.speedY *= 3;
     }
@@ -148,7 +232,7 @@ class Particle {
   }
 
   display() {
-    fill(255, this.lifespan);
+    fill(this.color, this.lifespan);
     noStroke();
     ellipse(this.x, this.y, this.size);
   }
@@ -162,17 +246,18 @@ class Trail {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.lifespan = 255;
+    this.alpha = 255;
+    this.size = random(5, 10);
   }
 
   display() {
-    fill(255, this.lifespan);
+    fill(255, this.alpha);
     noStroke();
-    ellipse(this.x, this.y, 10);
-    this.lifespan -= 5;
+    ellipse(this.x, this.y, this.size);
+    this.alpha -= 5;
   }
 
   isFinished() {
-    return this.lifespan < 0;
+    return this.alpha < 0;
   }
 }
