7b220783d900683868f5746244c4cb64f938808c
Román Ríos <romanrios@live.com>
2024-10-12 22:10:25 -0300

    pong text-to-speech

--- pong/sketch.js
+++ pong/sketch.js
@@ -10,12 +10,14 @@ let particles = [];
 let trail = [];
 let explosionParticles = [];
 let bg;
-let bounceSound, gameOverSound, scoreSound;
+let bounceSound, scoreSound;
 let gameEnded = false;
 let inputBlocked = false;
 let gameOverMessage = "";
 let gameOverTimer = 0;
 let ballAngle = 0;
+
+const TARGET_SCORE = 3;
 
 function preload() {
   ball = loadImage('assets/bola.png');
@@ -23,7 +25,6 @@ function preload() {
   computerPaddle = loadImage('assets/barra2.png');
   bg = loadImage('assets/fondo1.png');
   bounceSound = loadSound('assets/bounce.wav');
-  gameOverSound = loadSound('assets/gameover.wav');
   scoreSound = loadSound('assets/score.mp3');
 }
 
@@ -35,7 +36,7 @@ function setup() {
 function draw() {
   imageMode(CORNER);
   image(bg, 0, 0, width, height);
-  
+
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
     particles[i].display();
@@ -70,13 +71,13 @@ function draw() {
   }
   image(playerPaddle, 25, playerPaddleY + paddleHeight / 2, paddleWidth, paddleHeight);
   image(computerPaddle, width - 25, computerPaddleY + paddleHeight / 2, paddleWidth, paddleHeight);
-  
+
   textSize(128);
   fill(255, 150);
   textAlign(CENTER, CENTER);
   text(playerScore, width / 4, height / 2);
   text(computerScore, 3 * width / 4, height / 2);
-  
+
   if (!gameEnded) {
     ballX += ballSpeedX;
     ballY += ballSpeedY;
@@ -113,24 +114,24 @@ function draw() {
       computerScore++;
       createExplosion(30, height / 2);
       scoreSound.play();
+      speakScore();
       resetBall();
     } else if (ballX > width) {
       playerScore++;
       createExplosion(width - 30, height / 2);
       scoreSound.play();
+      speakScore();
       resetBall();
     }
 
-    if (playerScore === 10) {
+    if (playerScore === TARGET_SCORE) {
       gameEnded = true;
-      gameOverSound.play();
-      gameOverMessage = "YOU WIN";
+      gameOverMessage = "¡Ganaste!";
       blockInput();
       gameOverTimer = millis();
-    } else if (computerScore === 10) {
+    } else if (computerScore === TARGET_SCORE) {
       gameEnded = true;
-      gameOverSound.play();
-      gameOverMessage = "YOU LOSE";
+      gameOverMessage = "¡Perdiste!";
       blockInput();
       gameOverTimer = millis();
     }
@@ -150,7 +151,7 @@ function draw() {
         computerPaddleY -= min(3, computerPaddleY - ballY + paddleHeight / 2);
       }
       computerPaddleY = constrain(computerPaddleY, 0, height - paddleHeight);
-      
+
       trail.push(new Trail(ballX, ballY));
     }
   } else {
@@ -208,6 +209,91 @@ function blockInput() {
     inputBlocked = false;
   }, 2000);
 }
+
+function speakScore() {
+  const victoryMessages = [
+    "¡Mirá vos, ganaste en el espacio!",
+    "¡Sorpresa! No pensé que lo lograrías.",
+    "¡Impresionante! Hasta la máquina se rinde.",
+    "¡Felicidades! La incompetencia se fue de viaje.",
+    "¡Increíble! Dominás hasta en gravedad cero.",
+    "¡Felicitaciones! Rompiste el récord del milagro.",
+    "¡Sos el rey! La máquina no puede más.",
+    "¡Bravo! Tu rival digital se retira.",
+    "¡Lo hiciste! Campeón del espacio.",
+    "¡Impresionante! Ni el algoritmo te vio venir.",
+    "Ganaste, pero no pareció justo.",
+    "¡Wow! Hoy sos el mejor de la galaxia.",
+    "¡Increíble! La máquina no supo qué hacer.",
+    "¡Felicidades! Esto parece real.",
+    "¡Sorpresa! Subestimado, como siempre.",
+    "¡Ganaste! Hora de volver a la Tierra.",
+    "¡Felicidades! Le ganaste a un montón de código.",
+    "¡Increíble! Hasta el universo está impresionado."
+];
+
+const defeatMessages = [
+  "¡Qué desastre! Intentalo de nuevo... o no.",
+  "¡Uff! La máquina te aplastó.",
+  "Lamentable. Hasta para el espacio.",
+  "Rey de las derrotas, al menos un título tenés.",
+  "Sos humano. No sorprende.",
+  "La máquina pide más competencia.",
+  "Perdiste ante un montón de ceros y unos.",
+  "¡Perdiste! Practicar no te vendría mal.",
+  "La máquina está festejando tu derrota.",
+  "¡Qué desastre! Mejoraste nada.",
+  "La máquina se ríe. Y con razón.",
+  "Perdiste. Esto ya aburre.",
+  "Hasta los astros te están juzgando."
+];
+
+const scoreMessages = [
+  `Va ${playerScore} a ${computerScore}. ¡Así se pierde!`,
+  `Estamos ${playerScore} a ${computerScore}. ¡No pierdas la fe!`,
+  `${playerScore} a ${computerScore}. ¡No pinta bien!`,
+  `${playerScore} a ${computerScore}. Todo mal.`,
+  `${playerScore} a ${computerScore}. ¡Qué flojo!`,
+  `${playerScore} a ${computerScore}. ¡Dramón!`,
+  `${playerScore} a ${computerScore}. ¡Una locura!`,
+  `${playerScore} a ${computerScore}. ¡No termina más!`,
+  `¡Sorpresa! ${playerScore} a ${computerScore}.`,
+  `¿Quién lo pensaría? ${playerScore} a ${computerScore}.`,
+  `Sigue así: ${playerScore} a ${computerScore}.`,
+  `¡Increíble! ${playerScore} a ${computerScore}.`,
+  `¿Vamos a ver? ${playerScore} a ${computerScore}.`,
+  `¿Qué pasa? ${playerScore} a ${computerScore}.`,
+  `${playerScore} a ${computerScore}. Esto ya está complicado.`,
+  `${playerScore} a ${computerScore}. Emergencia.`,
+  `${playerScore} a ${computerScore}. ¡Qué sorpresa!`,
+  `${playerScore} a ${computerScore}. Se pone interesante.`,
+  `${playerScore} a ${computerScore}. O no.`,
+  `${playerScore} a ${computerScore}. ¿En serio?`,
+  `${playerScore} a ${computerScore}. ¡Cuidado!`,
+  `${playerScore} a ${computerScore}. ¡Batalla épica!`,
+  `${playerScore} a ${computerScore}. Ni en el espacio se ve esto.`,
+  `${playerScore} a ${computerScore}. El juego se alarga.`,
+  `${playerScore} a ${computerScore}. ¿Quién sigue?`,
+  `${playerScore} a ${computerScore}. Esto no termina más.`,
+  `${playerScore} a ${computerScore}. Y sigue el conteo.`,
+  `${playerScore} a ${computerScore}. ¿Lo ves venir?`
+];
+
+
+
+  let message;
+  if (playerScore === TARGET_SCORE) {
+    message = victoryMessages[Math.floor(Math.random() * victoryMessages.length)];
+  } else if (computerScore === TARGET_SCORE) {
+    message = defeatMessages[Math.floor(Math.random() * defeatMessages.length)];
+  } else {
+    message = scoreMessages[Math.floor(Math.random() * scoreMessages.length)];
+  }
+
+  let utterance = new SpeechSynthesisUtterance(message);
+  window.speechSynthesis.speak(utterance);
+}
+
 
 class Particle {
   constructor(x, y, isExplosion = false) {
