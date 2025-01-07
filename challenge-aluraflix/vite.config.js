8ad37da7e1c19370265c44760d53d2f1fa7784d0
Román Ríos <romanrios@live.com>
2025-01-07 00:23:10 -0300

    url test

--- challenge-aluraflix/vite.config.js
+++ challenge-aluraflix/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://romanrios.github.io/oracle-next-education/challenge-aluraflix/dist/",
   plugins: [react()],
-})
+});
--- react-org/vite.config.js
+++ react-org/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/oracle-next-education/challenge-aluraflix/dist/",
   plugins: [react()],
 });
